8e04ef4d7ec3a0048f7dc73a21e053c7cd118ea5
yilberpabon-droid <yilberpabin@gmail.com>
2025-10-21 09:01:13 -0500

    proyecto base 19

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_pabon_yilber.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
