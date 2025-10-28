5de9a361c2a08d79aa059dc555a8542fd504e05c
yilberpabon-droid <yilberpabin@gmail.com>
2025-10-28 11:41:32 -0500

    Segundo Commit

--- implementacion/UsuarioServices.cs
+++ implementacion/UsuarioServices.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using proyecto_pabon_yilber.Data;
+using proyecto_pabon_yilber.Models;
+using proyecto_pabon_yilber.services;
 
 namespace proyecto_pabon_yilber.implementacion
 {
-    public class UsuarioServices
+    public class UsuarioServices : IUsuarioService
     {
-        
+        private readonly dbcontex dbContext;
+
+        public UsuarioServices(dbcontex dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async void CrearUsuario(Usuariomodel pitufo)
+        {
+            if (pitufo != null)
+            {
+                dbContext.Usuarios.Add(pitufo);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
     }
 }
--- services/IUsuario.cs
+++ services/IUsuario.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using proyecto_pabon_yilber.Models;
 
 namespace proyecto_pabon_yilber.services
 {
-    public interface IUsuario
+    public interface IUsuarioService
     {
-        
+        void CrearUsuario(Usuariomodel pitufo);
     }
 }
